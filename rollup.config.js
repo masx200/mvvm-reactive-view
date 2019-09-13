64b07d47cbb2c6e061ec166df621e7e958b54cb3
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-13 16:38:38 +0800

    搭建结构

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import json from "rollup-plugin-json";
+const myterserplugin = terser({
+  sourcemap: true,
+  toplevel: true,
+  output: {
+    comments: !1,
+    ascii_only: !0
+  },
+  compress: {
+    drop_console: true,
+    drop_debugger: true,
+    pure_funcs: ["console.log"]
+  },
+  mangle: { properties: false }
+});
+export default [
+  {
+    input: "./src/index.js",
+    output: [
+      {
+        file: "./dist/index.js",
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [json(), resolve(), commonjs()]
+  },
+  {
+    input: "./dist/index.esm.js",
+    output: [
+      {
+        file: "./dist/index.min.js",
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [json(), resolve(), commonjs(), myterserplugin]
+  }
+];
