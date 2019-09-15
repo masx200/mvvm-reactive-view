b5399632ee85d356707c0daf5740945272c6d6a0
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-15 10:12:49 +0800

    Update createstate.ts

--- src/createstate.ts
+++ src/createstate.ts
@@ -17,6 +17,36 @@ export default function(init: string | number | boolean | undefined) {
       }
     });
   } else if (isobject(init)) {
+    return new Proxy(new ReactiveState(init), {
+      get(target, key) {
+        const value = Reflect.get(target, "value");
+        if (Reflect.has(target, key)) {
+          return Reflect.get(target, key);
+        } else if (Reflect.has(value, key)) {
+          /*  */
+          return Reflect.get(target, key);
+        }
+      },
+      ownKeys(target) {
+        return Array.from(
+          new Set([
+            ...Reflect.ownKeys(target),
+            ...Reflect.ownKeys(Reflect.get(target, "value"))
+          ])
+        );
+      },
+      set(target, key, value) {
+        if (key === "value" && isobject(value)) {
+          // if (target[key] !== value) {
+          Reflect.set(target, key, value);
+          target[dispatchsymbol]();
+          // }
+          return true;
+        } else {
+          return false;
+        }
+      }
+    });
   } else {
     throw TypeError("invalid State");
   }
