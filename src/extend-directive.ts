ae3c79f4de9762066204fea4fbc50aabff25edfe
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-16 07:57:30 +0800

    Update extend-directive.ts

--- src/extend-directive.ts
+++ src/extend-directive.ts
@@ -7,8 +7,18 @@ options
 ){
 
 Object.entries(options).forEach(([key,value])=>{
+if(typeof value!=="function"){
+
+throw TypeError("invalid directive")
+
+}else{
+
 
 directives[key]=value
+
+}
+
+
 })
 
 
