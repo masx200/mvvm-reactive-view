bf69d82825bdc54885dfe749ef66d81e7f328667
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-15 09:40:32 +0800

    Update createstate.ts

--- src/createstate.ts
+++ src/createstate.ts
@@ -1,17 +1,19 @@
 import isprimitive from "./isprimitive";
 import Primitivestate, { dispatchsymbol } from "./primitivestate";
 export default function(init: string | number | boolean | undefined) {
-  return new Proxy(new Primitivestate(init), {
-    set(target, key, value) {
-      if (key === "value" && isprimitive(value)) {
-        // if (target[key] !== value) {
-        Reflect.set(target, key, value);
-        target[dispatchsymbol]();
-        // }
-        return true;
-      } else {
-        return false;
+  if (isprimitive(init)) {
+    return new Proxy(new Primitivestate(init), {
+      set(target, key, value) {
+        if (key === "value" && isprimitive(value)) {
+          // if (target[key] !== value) {
+          Reflect.set(target, key, value);
+          target[dispatchsymbol]();
+          // }
+          return true;
+        } else {
+          return false;
+        }
       }
-    }
-  });
+    });
+  }
 }
