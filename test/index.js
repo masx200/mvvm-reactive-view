2278f3132d95a4443a15a8b5aaec718ab4d2f657
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-16 23:37:50 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -306,23 +306,27 @@ import {
       <div></div>
     </div>
     <h1>
-      <svg
-        style="
-    width:100%;
-    height: 200px;
-"
-        class="octicon octicon-book"
-        viewBox="0 0 16 16"
-        version="1.1"
-        width="16"
-        height="16"
-        aria-hidden="true"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z"
-        ></path></svg
-      ><svg
+      
+<svg xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink"
+     version="1.1"
+     baseProfile="full"
+
+style="
+    width:600px;
+    height: 600px;
+">
+  <g fill-opacity="0.7" stroke="black" stroke-width="0.1cm">
+    <circle cx="6cm" cy="2cm" r="100" fill="red"
+                    transform="translate(0,50)" />
+    <circle cx="6cm" cy="2cm" r="100" fill="blue"
+                    transform="translate(70,150)" />
+    <circle cx="6cm" cy="2cm" r="100" fill="green"
+                    transform="translate(-70,150)" />
+  </g>
+</svg>
+
+<svg
         style="
     width: 100%;
     height: 200px;
