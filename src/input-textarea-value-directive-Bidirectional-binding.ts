93d850c0c19ccaa6360d3269f38133daffbc8932
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-16 09:33:04 +0800

    Update input-textarea-value-directive-Bidirectional-binding.ts

--- src/input-textarea-value-directive-Bidirectional-binding.ts
+++ src/input-textarea-value-directive-Bidirectional-binding.ts
@@ -6,5 +6,18 @@ import directives from "./extend-directive.ts"
 
 directives({value(element,value,vdom){
 
+if(value instanceof ReactiveState &&(vdom.type==="input"||vdom.type==="textarea"))
+{
 
+
+
+
+}else{
+
+
+
+throw TypeError("invalid ReactiveState or element")
+
+
+}
 }})
