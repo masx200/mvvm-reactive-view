db061ac019bb7b887eeca22d0b842ec6ecbad644
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-19 22:53:34 +0800

    Update Object-fromEntries.ts

--- src/Object-fromEntries.ts
+++ src/Object-fromEntries.ts
@@ -1,3 +1,9 @@
 import fromPairs from "lodash/fromPairs.js";
 // console.log(fromPairs);
-if ("function" !== typeof Object.fromEntries) Object.fromEntries = fromPairs;
+if ("function" !== typeof Object.fromEntries) 
+
+{
+
+Object.fromEntries = fromPairs;
+
+}
