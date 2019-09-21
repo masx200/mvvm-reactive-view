d365a0edec30994dd92d44e9643b186996069451
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-21 10:46:08 +0800

    attributeChangedCallback

--- src/attrchange.ts
+++ src/attrchange.ts
@@ -1,6 +1,31 @@
+const attributeChangedCallback = "attributeChangedCallback";
+import { isFunction } from "./util";
 export class AttrChange extends HTMLElement {
+  /* disconnectedCallback() {
+  console.log('Custom square element removed from page.');
+} */
+  /* connectedCallback() {
+  console.log('Custom square element added to page.');
+  updateStyle(this);
+} */
   //
   //   connectedCallback(){}
-  setAttribute() {}
-  removeAttribute() {}
+  /* attributeChangedCallback(name, oldValue, newValue) {
+  console.log('Custom square element attributes changed.');
+  updateStyle(this);
+} */
+  setAttribute(qualifiedName: string, value: string) {
+    const oldValue = super.getAttribute(qualifiedName);
+    super.setAttribute(qualifiedName, value);
+    if (isFunction(this[attributeChangedCallback])) {
+      this[attributeChangedCallback](qualifiedName, oldValue, value);
+    }
+  }
+  removeAttribute(qualifiedName: string) {
+    const oldValue = super.getAttribute(qualifiedName);
+    super.removeAttribute(qualifiedName);
+    if (isFunction(this[attributeChangedCallback])) {
+      this[attributeChangedCallback](qualifiedName, oldValue, undefined);
+    }
+  }
 }
