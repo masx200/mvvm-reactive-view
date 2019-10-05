0b786a732690213ce53291175b5951207579c529
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-05 16:05:00 +0800

    Update assetbrowser.ts

--- src/assetbrowser.ts
+++ src/assetbrowser.ts
@@ -4,11 +4,16 @@ if (
   //   typeof HTMLElement !== "function" ||
   !isfunction(window.Proxy) ||
   //   typeof Proxy !== "function" ||
-  !isobject(window.customElements) ||
+  !isobject(window.customElements) 
+/*||
   //   typeof customElements !== "object" ||
   !isfunction(window.CustomElementRegistry)
   //||
   //   typeof CustomElementRegistry !== "function"
+
+*/
 ) {
-  throw new TypeError(" browser not supported !");
+
+console.error("Proxy,HTMLElement ,customElements ,browser not supported !")
+  throw new TypeError();
 }
