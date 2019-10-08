2bc462f9d7c51bba95f18d9893f4929ba9286984
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-09 00:47:46 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,89 +1,66 @@
-import typescriptlib from "typescript";
-// import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import json from "rollup-plugin-json";
-import typescriptplugin from "rollup-plugin-typescript2";
-
+import typescriptlib from 'typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import json from 'rollup-plugin-json';
+import typescriptplugin from 'rollup-plugin-typescript2';
 const banner = `const {CustomEvent,requestAnimationFrame,URL,Blob,Element,Node,String,Array,document,Object,Reflect,Proxy,Symbol,Boolean,Promise,Set,Math,Error,TypeError,EventTarget,JSON,Map,window,clearTimeout,setTimeout,parseInt,globalThis ,self ,global }=Function('return this')();`;
-
 const myterserplugin = terser({
-  sourcemap: true,
-  toplevel: true,
-  output: {
-    comments: !1,
-    ascii_only: !0
-  },
-  compress: {
+    sourcemap: true,
     toplevel: true,
-    unused: true,
-    drop_console: true,
-    drop_debugger: true,
-    pure_funcs: ["console.log"]
-  },
-  mangle: { properties: false }
+    output: {
+        comments: !1,
+        ascii_only: !0
+    },
+    compress: {
+        toplevel: true,
+        unused: true,
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log']
+    },
+    mangle: { properties: false }
 });
 export default [
-  {
-    input: "./src/index.ts",
-    output: [
-      { banner, file: "./dist/index.js", format: "esm", sourcemap: true }
-    ],
-    plugins: [
-      json(),
-      resolve(),
-      commonjs(),
-      typescriptplugin({
-        tsconfig: "tsconfig.json",
-        typescript: typescriptlib
-      }),
-      terser({
-        sourcemap: true,
-        compress: false,
-        mangle: false,
-        output: {
-          ascii_only: !0,
-          comments: !1,
-          beautify: true
-        }
-      })
-    ]
-  },
-  {
-    input: "./dist/index.js",
-    output: [
-      {
-        file: "./dist/index.min.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [resolve(), commonjs(), myterserplugin]
-  }
-  /*   {
-    input: "./test/index.js",
-    output: [
-      {
-        file: "./test/index-es5.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      terser({
-        compress: false,
-        mangle: false,
-        output: {
-          ecma: 5,
-          comments: !1,
-          beautify: true
-        }
-      }),
-      babel({
-        plugins: ["@babel/plugin-proposal-class-properties"],
-        presets: ["@babel/preset-env"]
-      })
-    ]
-  } */
+    {
+        input: './src/index.ts',
+        output: [{
+                banner,
+                file: './dist/index.js',
+                format: 'esm',
+                sourcemap: true
+            }],
+        plugins: [
+            json(),
+            resolve(),
+            commonjs(),
+            typescriptplugin({
+                tsconfig: 'tsconfig.json',
+                typescript: typescriptlib
+            }),
+            terser({
+                sourcemap: true,
+                compress: false,
+                mangle: false,
+                output: {
+                    ascii_only: !0,
+                    comments: !1,
+                    beautify: true
+                }
+            })
+        ]
+    },
+    {
+        input: './dist/index.js',
+        output: [{
+                file: './dist/index.min.js',
+                format: 'esm',
+                sourcemap: true
+            }],
+        plugins: [
+            resolve(),
+            commonjs(),
+            myterserplugin
+        ]
+    }
 ];
