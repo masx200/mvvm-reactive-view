65a1bf61ffafaf767e30014b564b32bbb27e3dcd
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-16 23:29:14 +0800

    const banner = `const {Date,RegExp,Event,CustomEvent,requestAnimationFrame,URL,Blob,Element,Node,String,Array,document,Object,Reflect,Proxy,Symbol,Boolean,Promise,Set,Math,Error,TypeError,EventTarget,JSON,Map,window,clearTimeout,setTimeout,parseInt,globalThis ,self ,global }=Function('return this')();`;

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import sourcemaps from "rollup-plugin-sourcemaps";
 import { terser } from "rollup-plugin-terser";
 import typescriptplugin from "rollup-plugin-typescript2";
 import typescriptlib from "typescript";
-const banner = `const {Event,CustomEvent,requestAnimationFrame,URL,Blob,Element,Node,String,Array,document,Object,Reflect,Proxy,Symbol,Boolean,Promise,Set,Math,Error,TypeError,EventTarget,JSON,Map,window,clearTimeout,setTimeout,parseInt,globalThis ,self ,global }=Function('return this')();`;
+const banner = `const {Date,RegExp,Event,CustomEvent,requestAnimationFrame,URL,Blob,Element,Node,String,Array,document,Object,Reflect,Proxy,Symbol,Boolean,Promise,Set,Math,Error,TypeError,EventTarget,JSON,Map,window,clearTimeout,setTimeout,parseInt,globalThis ,self ,global }=Function('return this')();`;
 const myterserplugin = terser({
   sourcemap: true,
   toplevel: true,
