f450e8da53181b034b6cc20383e49dcbff69b575
masx200 <34191203+masx200@users.noreply.github.com>
2020-03-04 22:36:32 +0800

    创建一个不能运行代码的script

--- src/RenderVirtual/render-vdom-to-real.ts
+++ src/RenderVirtual/render-vdom-to-real.ts
@@ -125,8 +125,9 @@ export default function render(
         if (typeof type === "string") {
             if (type === "script") {
                 /* 禁止加载脚本 */
-
-                return createDocumentFragment();
+                /* 创建一个不能运行代码的script */
+                return createElementNS("never", "script");
+                // return createDocumentFragment();
             } else if (type === "svg") {
                 /* 没想到svg的创建方式这么特别?否则显示不出svg */
                 element = createsvgelement();
