b1f6895ac52a5743bb06f17cba322e1b86cf7ea4
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-21 16:14:21 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
+import CSS from "csstype";
+import { GetParentType, Listener } from "lib";
 import Condition from "./AttributeClass/conditon";
 import createComponent from "./AttributeClass/createComponent";
 import Switchable from "./AttributeClass/switchable";
-import h, { ElementAttributes } from "./CreateElement/create-element";
+import h, {
+    ATTRFOR,
+    classprop,
+    ElementAttributes,
+    styleprop
+} from "./CreateElement/create-element";
 import html from "./CreateElement/html";
+import { VaildVDom } from "./CreateElement/isvalidvdom";
 import Virtualdom, { Vdomchildren } from "./CreateElement/VirtualElement";
 import { Custom, Htmlelementconstructor } from "./CustomClass/customclass";
 import "./CustomClass/CustomElementRegistry";
 import "./Directives/built-in-directs";
-import Directives, { Extendfun } from "./Directives/extend-directive";
+import Directives, {
+    extenddirectives,
+    Extendfun
+} from "./Directives/extend-directive";
 import { useCreated } from "./life-cycle-context/use-created";
 import { useUpdated } from "./life-cycle-context/use-updated";
 import { useMounted } from "./life-cycle-context/useMounted";
@@ -15,10 +26,15 @@ import { useUnMounted } from "./life-cycle-context/useUnMounted";
 import MountElement from "./MountElement/MountElement";
 import "./others/assetbrowser";
 import createRef, { Ref } from "./others/createref";
-import computed from "./Reactivity/computed";
+import computed, { SetterFun } from "./Reactivity/computed";
 import createState, { StateType } from "./Reactivity/create-state";
-import watch from "./Reactivity/watch";
+import { gettercallback } from "./Reactivity/gettercallback";
+import ReactiveState from "./Reactivity/reactivestate";
+import { UnWrapArray } from "./Reactivity/UnWrapArray";
+import { UnWrapState } from "./Reactivity/unwrapstate";
+import watch, { CancelWatchfun } from "./Reactivity/watch";
 import render from "./RenderVirtual/render-vdom-to-real";
+import { Primitivetype } from "./UtilTools/isprimitive";
 export { Custom };
 export { StateType };
 export { render };
@@ -41,3 +57,21 @@ export { Virtualdom };
 export { Extendfun };
 export { Ref };
 export { ElementAttributes };
+export {
+    VaildVDom,
+    styleprop,
+    classprop,
+    ATTRFOR,
+    extenddirectives,
+    ReactiveState,
+    gettercallback,
+    UnWrapState,
+    SetterFun,
+    UnWrapArray,
+    GetParentType,
+    CancelWatchfun,
+    Listener,
+    Primitivetype,
+    CSSProperties
+};
+type CSSProperties = CSS.Properties<string | number>;
